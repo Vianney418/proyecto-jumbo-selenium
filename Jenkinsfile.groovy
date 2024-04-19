5cdb1a042dbe426a0e703aea3b97870f85aa71cf
viann <vianneyperdomo16@gmail.com>
2024-04-19 16:06:18 -0400

    se modifico el jenkinsfile para que se ejecuten los 2 test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -22,7 +22,7 @@ pipeline{
                 script{
                     def MAVEN_HOME = tool name: 'maven_3_9_6', type: 'maven'
                     withEnv (["PATH+MAVEN=${MAVEN_HOME}/bin"]) {
-                        bat "${MAVEN_HOME}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@TEST2\""
+                        bat "${MAVEN_HOME}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@TEST1 or @TEST2\""
                     }
                 }
             }
