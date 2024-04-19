34a1d5b495a2615c8509b9134f2fdb8df2b68da9
viann <vianneyperdomo16@gmail.com>
2024-04-19 12:20:25 -0400

    se agregaron instrucciones al archivo JenkinsFile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,39 @@
+pipeline{
+
+    agent any
+
+    stages{
+
+        stage('Compile Stage') {
+
+            steps {
+                script {
+                    def mvnHome = tool name: 'maven_3_9_6', type: 'maven'
+                    withEnv(["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean compile"
+                    }
+                }
+            }
+        }
+
+
+        stage('Test Stage'){
+            steps {
+                script{
+                    def mvnHome = tool name: 'maven_3_9_6', type: 'maven'
+                    withEnv (["PATH+MAVEN=${mvnHome}/bin"]) {
+                        bat "${mvnHome}\\bin\\mvn clean verify -Dcucumber.filter.tags=\"@TEST2\""
+                    }
+                }
+            }
+        }
+
+        stage ('Cucumber Reports'){
+            steps{
+                cucumber buildStatus: "UNSTABLE",
+                fileIncludePattern: "**/cucumber.json",
+                jsonReportDirectory: 'target'
+            }
+        }
+    }
+}
